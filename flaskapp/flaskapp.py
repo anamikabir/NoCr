ad3a3a00e8c70545412eb72aaca807e12b55b4ac
Anamika <anamikabir91@yahoo.in>
2018-01-22 12:03:35 -0800

    Added 2 scheduled jobs

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -5,20 +5,32 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
 # Sample schedular app 
-def print_date_time():
+def send_notifications():
     print time.strftime("%A, %d. %B %Y %I:%M:%S %p")
+
+def del_old_contents():
+    print "Running second job at a different interval"
 
 app = Flask(__name__)
 
-# return "Running FLASK Schedular"
 scheduler = BackgroundScheduler()
-scheduler.start()
+#scheduler.start()
 scheduler.add_job(
-    func=print_date_time,
+    func=send_notifications,
     trigger=IntervalTrigger(seconds=10),
-    id='printing_job',
-    name='Print date and time every five seconds',
+    id='notification_job',
+    name='Send notification for new posts every 30 mins',
     replace_existing=True)
+
+scheduler.add_job(
+    func=del_old_contents,
+    trigger=IntervalTrigger(seconds=15),
+    id='del_old_dict_job',
+    name='Delete older post enteries - runs once every 24 hours',
+    replace_existing=True)
+
+scheduler.start()
+
 # Shut down the scheduler when exiting the app
 atexit.register(lambda: scheduler.shutdown())
 
