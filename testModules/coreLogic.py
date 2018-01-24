78b67259e60075c0c0be6ffb44962ec44fe0dc20
Anamika <anamikabir91@yahoo.in>
2018-01-23 17:19:44 -0800

    Added feature - deleting old post entries

--- testModules/coreLogic.py
+++ testModules/coreLogic.py
@@ -5,15 +5,42 @@ OurList = {}
 
 def get_new_posts(theUrl):
     res =  Get_All_Listings(theUrl)
-    for posts in res:
-        print str(posts)+"\n"
-    # Get current date so application can delete enteries belong to previous day
     DateToday = datetime.datetime.now()
     currDate = DateToday.day
+    for posts in res:
+        if posts['postid'] in OurList:
+            print "Nothing happened"
+        else:
+            OurList[posts['postid']]= currDate
+            #print "This happened"
+            # send email operation
+    print str(OurList)
+    # Get current date so application can delete enteries belong to previous day
     #currMonth = DateToday.month 
     #print str(currDate)+ " " + str(currMonth)
     
 
+
+
+def del_old_entries():
+    D = {}
+    DateToday = datetime.datetime.now()
+    currDate = 12
+    
+    for k,v in OurList.iteritems():
+        if v == currDate:
+           D[k]=v
+    return D
+
+"""
+zc = raw_input("Enter Zipcode of the location: ")
+print zc
+"""
+
 theUrl = "https://sfbay.craigslist.org/search/sfc/roo?hasPic=1&postedToday=1&bundleDuplicates=1&search_distance=2&postal=94103&min_price=800&max_price=1800&availabilityMode=0&private_room=1"
 
 get_new_posts(theUrl)
+OurList['2133788862']=12
+OurList = del_old_entries()
+print str(OurList)
+
