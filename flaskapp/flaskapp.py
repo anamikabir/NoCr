5f89b1a9336ec9d956f29647580a5def847a74ea
Anamika <anamikabir91@yahoo.in>
2018-01-24 12:52:41 -0800

    Added functionailty to generate notifications for new posts

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -1,12 +1,46 @@
 import time
 import atexit
+
 from flask import Flask
+
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
-# Sample schedular app 
+#To import html parsing code to retrieve listings
+from htmlScraper import Get_All_Listings
+
+#To import functionality to create and send an email notification
+from emailHelper import email_helper
+
+
+currentListings = {}
+
+theUrl = "https://sfbay.craigslist.org/search/sfc/roo?hasPic=1&postedToday=1&bundleDuplicates=1&search_distance=2&postal=94103&min_price=800&max_price=1800&availabilityMode=0&private_room=1"
+
+# Function to send notifications for every new post
 def send_notifications():
-    print time.strftime("%A, %d. %B %Y %I:%M:%S %p")
+    res =  Get_All_Listings(theUrl)
+    DateToday = datetime.datetime.now()
+    currDate = DateToday.day
+    for posts in res:
+        if posts['postid'] in OurList:
+            OurList[posts['postid']]= currDate
+            #print "Nothing happened"
+        else:
+            OurList[posts['postid']]= currDate
+            #print "Email notification"
+            myEmailHelper = email_helper()
+            BodyContents = "Hi {}!\n\nThis is a notification email.\n\n"
+            EmailSubject = posts['title']
+            BodyContents + = posts['title']+"\n"
+            if 'price' in posts:
+                BodyContents + = "Rent (Price in USD): "+posts['price']+"\n"
+            if 'hood' in posts:
+                BodyContents + = "Neighbourhood: "+posts['hood']+"\n"
+            BodyContents + = "\nLink to Follow: " +posts['url']+"\n\nRegards,\nNoCr Notification Team\n"
+            #Sending the email
+            myEmailHelper.create_mail_alert(EmailSubject,BodyContents)
+
 
 def del_old_contents():
     print "Running second job at a different interval"
@@ -17,7 +51,7 @@ scheduler = BackgroundScheduler()
 #scheduler.start()
 scheduler.add_job(
     func=send_notifications,
-    trigger=IntervalTrigger(seconds=10),
+    trigger=IntervalTrigger(minutes=5),
     id='notification_job',
     name='Send notification for new posts every 30 mins',
     replace_existing=True)
