5617638b9d20061911d9fb9c718501008f03939c
Anamika <anamikabir91@yahoo.in>
2018-01-25 10:56:20 -0800

    Utilizing dictReader to make data persist in case of application shutdown

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -7,6 +7,9 @@ from flask import Flask
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
+#To import read and write dictionary functionality using pickle
+from dictReader import *
+
 #To import html parsing code to retrieve listings
 from htmlScraper import Get_All_Listings
 
@@ -16,7 +19,9 @@ from emailHelper import email_helper
 import logging
 logging.basicConfig()
 
-currentListings = {}
+ListingFile = "PostList.txt"
+
+currentListings = readfile(ListingFile)
 
 theUrl = "https://sfbay.craigslist.org/search/sfc/roo?hasPic=1&postedToday=1&bundleDuplicates=1&search_distance=2&postal=94103&min_price=800&max_price=1800&availabilityMode=0&private_room=1"
 
@@ -25,15 +30,14 @@ def send_notifications():
     res =  Get_All_Listings(theUrl)
     DateToday = datetime.datetime.now()
     currDate = DateToday.day
-    print len(res)
+    #print len(res)
     for posts in res:
         if posts['postid'] in currentListings:
             currentListings[posts['postid']]= currDate
-            print "Nothing happened"
+            #print "Nothing happened"
         else:
             currentListings[posts['postid']]= currDate
-            print "Email notification"
-            """
+            #print "Email notification"
             myEmailHelper = email_helper()
             BodyContents = "Hi {}!\n\nThis is a notification email.\n\n"
             EmailSubject = posts['title']
@@ -43,14 +47,22 @@ def send_notifications():
             if 'hood' in posts:
                 BodyContents += "Neighbourhood: "+posts['hood']+"\n"
             BodyContents += "\nLink to Follow: " +posts['url']+"\n\nRegards,\nNoCr Notification Team\n"
-            print "Sending the email"+ EmailSubject
+            #print "Sending the email"+ EmailSubject
             myEmailHelper.create_mail_alert(EmailSubject,BodyContents)
-            """
-    print "Done with all records"
-    print str(currentListings)
+    #print "Done with all records"
+    writefile(ListingFile,currentListings)
 
 def del_old_contents():
-    print "Running second job at a different interval"
+    D = {}
+    DateToday = datetime.datetime.now()
+    currDate = DateToday.day
+    
+    for k,v in currentListings.iteritems():
+        if v == currDate:
+           D[k]=v
+    currentListings = D
+    writefile(ListingFile,currentListings)
+
 
 app = Flask(__name__)
 
